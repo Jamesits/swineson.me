057f27bd502cd4d52f4a30851bbaca032b7ed589
James Swineson <github@public.swineson.me>
2017-05-11 15:40:05 +0800

    Change implementation of current year in footer

---
+++ assets/js/footnote-year.js
@@ -0,0 +1,15 @@
+(function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn);
+    } else {
+        document.attachEvent('onreadystatechange', function() {
+            if (document.readyState != 'loading')
+                fn();
+        });
+    }
+})(function() {
+    var d = new Date();
+    document.getElementById('current-year').innerHTML = d.getFullYear();
+});
