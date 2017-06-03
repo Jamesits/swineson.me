99efa22194281eb6e6ad9430bf6c0a35f400b07b
James Swineson <i@swineson.me>
2017-06-03 23:16:54 +0800

    Use transparent background

---
+++ assets/js/homepage.js
@@ -0,0 +1,9 @@
+(function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+})(function(){
+    var today=new Date,flag=!1;5==today.getUTCMonth()&&(3==today.getUTCDate()?today.getUTCHours()>=16&&(flag=!0):4==today.getUTCDate()&&today.getUTCHours()<16&&(flag=!0)),flag&&(document.querySelector(".home-template .main-header").style.backgroundColor="rgb(137, 6, 4)");
+});
