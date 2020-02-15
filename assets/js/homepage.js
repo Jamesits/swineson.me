43c5306a693a80d15c86d9d1e2912752bea0f461
James Swineson <github@public.swineson.me>
2020-02-15 23:41:29 +0800

    remove special image

--- assets/js/homepage.js
+++
@@ -1,9 +0,0 @@
-(function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-})(function(){
-    var today=new Date,flag=!1;5==today.getUTCMonth()&&(3==today.getUTCDate()?today.getUTCHours()>=16&&(flag=!0):4==today.getUTCDate()&&today.getUTCHours()<16&&(flag=!0)),flag&&(document.querySelector(".home-template .main-header").style.backgroundImage="url(/assets-nonfree/images/background-sp.png");
-});
